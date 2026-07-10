594f2dbccf864b21d9e010debd40db8821e18b6f
Toke <toke@kogama.com>
2026-07-10 21:05:32 +0200

    pdf export ordering

--- shoggoth/pdf_exporter.py
+++ shoggoth/pdf_exporter.py
@@ -159,6 +159,8 @@ def export(cards, target_file, image_folder, size=None, format='png', include_ba
     if size is None:
         size = EXPORT_SIZES[0][1]
 
+    cards = cards.sort(key=lambda x: x.project_number)
+
     target_folder = Path(target_file).parent
     temp_file = target_folder / '_temp.html'
 
@@ -179,6 +181,8 @@ def create_mbprint_pdf(cards, target_file, image_folder):
     prince_cmd, prince_cwd = _resolve_prince()
     if prince_cmd is None:
         raise Exception("can't export without prince")
+
+    cards = cards.sort(key=lambda x: x.project_number)
 
     target_folder = Path(target_file).parent
     temp_file = target_folder / '_temp.html'
@@ -201,6 +205,7 @@ def azao_pdf(cards, target_file_front, target_file_back, image_folder):
     prince_cmd, prince_cwd = _resolve_prince()
     if prince_cmd is None:
         raise Exception("can't export without prince")
+    cards = cards.sort(key=lambda x: x.project_number)
 
     target_folder = Path(target_file_front).parent
     temp_file = target_folder / '_temp.html'
